4d6b5c72e53133ade8cec323432ec6552183cf80
git4res <akuzin20@gmail.com>
2022-06-08 17:16:17 +0300

    project updated with routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,28 @@ import React, { Component } from "react";
 import {Header} from "./Header";
 import { ThemeProvider } from '@material-ui/styles';
 import theme from "./../common/MUITheme";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
             <ThemeProvider theme={theme}>
-                <Header/>
+                <BrowserRouter>
+                    <>
+                        <Header/>
+                        <Switch>
+                            <Route exact path='/' component={() => <div>Home</div>} />
+                            <Route exact path='/services' component={() => <div>services</div>} />
+                            <Route exact path='/custom-software' component={() => <div>custom-software</div>} />
+                            <Route exact path='/mobile-apps' component={() => <div>mobile-apps</div>} />
+                            <Route exact path='/websites' component={() => <div>websites</div>} />
+                            <Route exact path='/vision' component={() => <div>vision</div>} />
+                            <Route exact path='/about' component={() => <div>about</div>} />
+                            <Route exact path='/contact' component={() => <div>contact</div>} />
+                            <Route exact path='/estimate' component={() => <div>estimate</div>} />
+                        </Switch>
+                    </>
+                </BrowserRouter>
             </ThemeProvider>
         )
     }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { makeStyles } from '@material-ui/core/styles';
@@ -12,6 +12,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
+import {Link} from 'react-router-dom';
 
 import logo from './../assets/logo-new.svg';
 
@@ -26,6 +27,9 @@ const useStyles = makeStyles((theme) => ({
     },
     tabsContainer: {
         marginLeft: 'auto'
+    },
+    toolBarMargin: {
+        ...theme.mixins.toolbar
     },
     tab: {
         ...theme.typography.tab,
@@ -69,25 +73,30 @@ function ScrollTop(props) {
 }
 
 export const Header = (props) => {
+    const [value, setValue] = useState(0);
     const classes = useStyles();
+
+    const onChangeHandle = (evt, value) => setValue(value);
+
     return (
         <>
             <CssBaseline/>
             <AppBar>
                 <Toolbar disableGutters>
                     <img src={logo} alt="company logo" className={classes.logo}/>
-                    <Tabs className={classes.tabsContainer}>
-                        <Tab className={classes.tab} label="Home"/>
-                        <Tab label="Services"/>
-                        <Tab label="Vision"/>
-                        <Tab label="About us"/>
-                        <Tab label="Contact us"/>
+                    <Tabs value={value} onChange={onChangeHandle} className={classes.tabsContainer}>
+                        <Tab className={classes.tab} component={Link} to="/" label="Home"/>
+                        <Tab label="Services" component={Link} to="/services"/>
+                        <Tab label="Vision" component={Link} to="/vision"/>
+                        <Tab label="About us" component={Link} to="/about"/>
+                        <Tab label="Contact us" component={Link} to="/contact"/>
                     </Tabs>
                     <Button className={classes.btn} variant='contained' color='secondary'>
                         Button
                     </Button>
                 </Toolbar>
             </AppBar>
+            <div className={classes.toolBarMargin}/>
             <Toolbar id="back-to-top-anchor"/>
             <Container>
                 <Box my={2}>
